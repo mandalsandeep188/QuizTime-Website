4a19ee38209d3fc458cefa2e33a5bd3937ca0db7
mandalsandeep188 <63152839+mandalsandeep188@users.noreply.github.com>
2020-04-27 13:40:17 +0530

    Quiz Website

--- responsive.js
+++
@@ -1,8 +0,0 @@
-function navbar()
-{
-    nav=document.getElementById("navbar");
-    if(nav.style.display=="block")
-        nav.style.display="none";
-    else
-        nav.style.display="block";
-}
