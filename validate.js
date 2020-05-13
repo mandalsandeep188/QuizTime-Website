e9b05c3f943bd82f60d01131c3ec9e161514dc63
mandalsandeep188 <63152839+mandalsandeep188@users.noreply.github.com>
2020-05-13 13:51:00 +0530

    validate

--- validate.js
+++ validate.js
@@ -8,11 +8,13 @@ function validate() {
     let res = true;
     if (username.value.length < 5 || username.value.length >= 12) {
         let user = document.getElementById('user');
+        username.style.borderColor="red";
         user.style.display = "block";
         res = false;
     }
     else {
         let user = document.getElementById('user');
+        username.style.borderColor="black";
         user.style.display = "none";
     }
     if (!male.checked && !female.checked) {
@@ -26,20 +28,24 @@ function validate() {
     }
     if (password.value.length < 8 || password.value.length > 12) {
         let pass = document.getElementById('pass');
+        password.style.borderColor="red";
         pass.style.display = "block";
         res = false;
     }
     else {
         let pass = document.getElementById('pass');
+        password.style.borderColor="black";
         pass.style.display = "none";
     }
     if (password.value != cnfpassword.value) {
         let cnfpass = document.getElementById('cnfpass');
+        cnfpassword.style.borderColor="red";
         cnfpass.style.display = "block";
         res = false;
     }
     else {
         let cnfpass = document.getElementById('cnfpass');
+        cnfpassword.style.borderColor="black";
         cnfpass.style.display = "none";
     }
     let atIndex = email.value.indexOf("@");
@@ -47,10 +53,12 @@ function validate() {
     if (atIndex < 1 || dotIndex >= email.value.length - 2 || dotIndex -
         atIndex < 3) {
         let emailid = document.getElementById('emailid');
+        email.style.borderColor="red";
         emailid.style.display = "block";
         res = false;
     } else {
         let emailid = document.getElementById('emailid');
+        email.style.borderColor="black";
         emailid.style.display = "none";
     }
     return res;
